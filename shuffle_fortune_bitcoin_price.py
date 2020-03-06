cc71cd9ac8c2a7898c34e33f03528ea7ba7f7e08
camilo <info@krrnk.com>
2020-03-06 12:30:03 +0200

    application

---
+++ shuffle_fortune_bitcoin_price.py
@@ -0,0 +1,52 @@
+import random
+import os
+import subprocess
+from forex_python.bitcoin import BtcConverter
+bitcoin = BtcConverter()
+random.seed(bitcoin.get_latest_price('EUR'))
+
+
+def read_cards():
+    print('Enter the amount of cards')
+    n_cards = int(input())
+    print('Enter the card numbers speareted by spaces')
+    card_number = input().replace(" ", "")
+    card_number_list = list(card_number)
+    if len(card_number_list) > n_cards:
+        print("ERROR: More numbers than cards")
+    else:
+        print(card_number_list)
+
+    selected_cards = []
+    if os.path.exists("gather_cards.txt"):
+        os.remove('gather_cards.txt')
+    
+    if os.path.exists("fortune.txt"):
+        os.remove('fortune.txt')
+
+    with open('gather_cards.txt', 'w+') as text_file:
+        for i in range(n_cards):
+            selected_cards.append(open('epavarmuus_cards/epavarmuus_' + str(card_number_list[i]) + '.txt', 'r'))
+            text_file.write(selected_cards[i].read())
+            selected_cards[i].close()
+        text_file.seek(0)
+        text_as_lines = text_file.readlines()
+        # print(text_as_lines)
+        for i in range(len(text_as_lines)):
+            text_as_lines[i] = text_as_lines[i].strip('\n')
+        random.shuffle(text_as_lines)
+        print(bitcoin.get_latest_price('EUR'))
+        print(text_as_lines)
+        with open('fortune.txt', 'w') as text_file:
+            text_file.write(str(text_as_lines))
+
+    subprocess.run(['open', 'fortune.txt'], check=True)
+
+    
+
+read_cards()
+
+
+    
+
+
